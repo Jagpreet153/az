49d2789ccefe47771fe7358d9b79ab7efe4d57a8
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-11-05 18:06:56 +0530

    fixed bug

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
-import Main from './components/main/Main';
 function App() {
   return (
     <div className="p-0 mr-[2rem] mt-[-0.5rem] flex">
-      <Sidebar />
-      {/* <Main /> */}
-    
+      <Sidebar />    
     </div>
   );
 }
